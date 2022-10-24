bb9b5fde26b21c9b33e466d4b1c832f45f82af3b
Maria <lysenina.maria@gmail.com>
2022-10-24 23:28:39 +0300

    fix hashrouter import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import {HashRouter, HashRouter} from "react-router-dom";
+import {HashRouter, BrowserRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
 
